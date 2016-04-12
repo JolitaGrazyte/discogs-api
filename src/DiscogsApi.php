d4ec5c9818c36bbd4ab3b75924dbc78b1f02f47f
Jolita Grazyte <jolita.grazyte@student.kdg.be>
2016-04-12 13:29:28 +0200

    add get functionality

--- src/DiscogsApi.php
+++ src/DiscogsApi.php
@@ -2,26 +2,55 @@
 
 namespace Jolita\DiscogsApiWrapper;
 
+use GuzzleHttp\Client;
+
 class DiscogsApi
 {
     protected $resources;
     protected $config;
+    protected $baseUrl;
 
     public function __construct()
     {
-        $this->resources = require(__DIR__.'../../config/laravel-discogs-api-resources.php');
-        $this->config = require(__DIR__.'../../config/laravel-discogs-api-config.php');
+        $this->resources = require(__DIR__.'../../config/discogs-resources.php');
+        $this->config = require(__DIR__.'../../config/discogs-api.php');
+        $this->baseUrl = 'https://api.discogs.com';
     }
 
-    /**
-     * Friendly welcome
-     *
-     * @param string $phrase Phrase to return
-     *
-     * @return string Returns the phrase passed in
-     */
-    public function echoPhrase($phrase)
+    public function get(string $resource, string $id = '', array $query = [])
     {
-        return $phrase;
+        $path = $this->path($id, $resource);
+
+        $content = (new Client())
+            ->get(
+                $this->url($path),
+                $this->parameters($query)
+            )
+            ->getBody()
+            ->getContents();
+
+        return json_decode($content);
+    }
+
+    protected function path(string $id, string $resource) : string
+    {
+        $resourcePath =  $this->resources[$resource];
+
+        return !empty($id) ? str_replace('{id}', $id, $resourcePath) : $resourcePath;
+    }
+
+    protected function parameters(array $query) : array
+    {
+        $token = $this->config['token'];
+
+        return [
+            'stream' => true,
+            'query' => array_add($query, 'token', $token)
+        ];
+    }
+
+    protected function url(string $path) : string
+    {
+        return "{$this->baseUrl}/{$path}";
     }
 }
--- tests/DiscogsApiTest.php
+++ tests/DiscogsApiTest.php
@@ -2,11 +2,34 @@
 
 namespace Jolita\DiscogsApiWrapper\Test;
 
+use Jolita\DiscogsApiWrapper\DiscogsApi;
+
 class DiscogsApiTest extends \PHPUnit_Framework_TestCase
 {
-    /** @test */
-    public function true_is_true()
+    protected $discogs;
+
+    public function setUp()
     {
-        $this->assertTrue(true);
+        parent::setUp();
+        $this->discogs = (new DiscogsApi());
     }
+
+    /** @test */
+    public function it_can_get_artist_by_id()
+    {
+        $nameMustBe = 'The Persuader';
+        $artist = $this->discogs->get('artist', 1);
+
+        $this->assertEquals($nameMustBe, $artist->name);
+    }
+
+    /** @test */
+    public function it_can_get_label_by_id()
+    {
+        $nameMustBe = 'Planet E';
+        $label = $this->discogs->get('label', 1);
+
+        $this->assertEquals($nameMustBe, $label->name);
+    }
+
 }
