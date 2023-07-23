04e179d9090706b5f64cecebb1ecdfeba3ff1ce8
Jolita Grazyte <jolita@jolitagraze.com>
2023-07-23 15:31:19 +0200

    update travis, php version, discogs response

--- src/Entities/DiscogsResponse.php
+++ src/Entities/DiscogsResponse.php
@@ -2,13 +2,15 @@
 
 namespace Jolita\DiscogsApi\Entities;
 
-class DiscogsResponse
+class DiscogsRelease
 {
     public int $id;
     public array $images;
     public array $artist;
+    public string $title;
     public array $labels;
     public array $formats;
-    public string $title;
+    public array $genres;
+    public array $styles;
 
 }
